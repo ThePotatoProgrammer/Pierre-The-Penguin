1d8c5cb6889c8f90fcfe136a87f98fc741601821
Joshua Baker <thepotatoprogrammer@gmail.com>
2022-05-07 23:02:10 -0500

    progress

--- Pierre The Penguin/GameScene.swift
+++ Pierre The Penguin/GameScene.swift
@@ -11,7 +11,21 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        // Debug info
-        self.calculateAccumulatedFrame()
+        // Make the scene position from its lower left corner, regardless of any other settings.
+        self.anchorPoint = .zero
+        
+        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+        mySprite.position = CGPoint(x: 150, y: 150)
+        mySprite.name = "square"
+        
+        self.addChild(mySprite)
+        
+//        let demoAction = SKAction.move(to: CGPoint(x: 300, y: 150), duration: 3)
+        let demoAction1 = SKAction.scale(to: 4, duration: 5)
+        let demoAction2 = SKAction.rotate(byAngle: 5, duration: 5)
+        let actionGroup = SKAction.group([demoAction1, demoAction2])
+        let actionSequence = SKAction.sequence([demoAction1, demoAction2])
+//        mySprite.run(actionGroup)
+        mySprite.run(actionSequence)
     }
 }
